a29d2638bf0fc72c5ecb329b7beb47404801dc5c
Haji ali <64609465+hajiali-bot@users.noreply.github.com>
2020-07-12 13:26:16 +0000

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -6,20 +6,38 @@ blue='\e[1;34m'
 red='\e[1;31m'
 yellow='\e[1;33m' 
 echo ""
-echo""
-banner() {
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  ██   ██ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   █  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██   █  ██   ██ ██ ██        \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██   ██ ██    ██ ███\e[0m''\e[1;33m\e[0m\e[1;32m ██████  ███████ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██      ██   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██ ███████   \e[0m'
-  echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.0] \e[0m'                                       
+
+                                                                                                             
+                                                                                                             
+HHHHHHHHH     HHHHHHHHH                   jjjj   iiii                      AAA               lllllll   iiii  
+H:::::::H     H:::::::H                  j::::j i::::i                    A:::A              l:::::l  i::::i 
+H:::::::H     H:::::::H                   jjjj   iiii                    A:::::A             l:::::l   iiii  
+HH::::::H     H::::::HH                                                 A:::::::A            l:::::l         
+  H:::::H     H:::::H    aaaaaaaaaaaaa  jjjjjjjiiiiiii                 A:::::::::A            l::::l iiiiiii 
+  H:::::H     H:::::H    a::::::::::::a j:::::ji:::::i                A:::::A:::::A           l::::l i:::::i 
+  H::::::HHHHH::::::H    aaaaaaaaa:::::a j::::j i::::i               A:::::A A:::::A          l::::l  i::::i 
+  H:::::::::::::::::H             a::::a j::::j i::::i              A:::::A   A:::::A         l::::l  i::::i 
+  H:::::::::::::::::H      aaaaaaa:::::a j::::j i::::i             A:::::A     A:::::A        l::::l  i::::i 
+  H::::::HHHHH::::::H    aa::::::::::::a j::::j i::::i            A:::::AAAAAAAAA:::::A       l::::l  i::::i 
+  H:::::H     H:::::H   a::::aaaa::::::a j::::j i::::i           A:::::::::::::::::::::A      l::::l  i::::i 
+  H:::::H     H:::::H  a::::a    a:::::a j::::j i::::i          A:::::AAAAAAAAAAAAA:::::A     l::::l  i::::i 
+HH::::::H     H::::::HHa::::a    a:::::a j::::ji::::::i        A:::::A             A:::::A   l::::::li::::::i
+H:::::::H     H:::::::Ha:::::aaaa::::::a j::::ji::::::i       A:::::A               A:::::A  l::::::li::::::i
+H:::::::H     H:::::::H a::::::::::aa:::aj::::ji::::::i      A:::::A                 A:::::A l::::::li::::::i
+HHHHHHHHH     HHHHHHHHH  aaaaaaaaaa  aaaaj::::jiiiiiiii     AAAAAAA                   AAAAAAAlllllllliiiiiiii
+                                         j::::j                                                              
+                               jjjj      j::::j                                                              
+                              j::::jj   j:::::j                                                              
+                              j::::::jjj::::::j                                                              
+                               jj::::::::::::j                                                               
+                                 jjj::::::jjj                                                                
+                                    jjjjjj                                                                   
+
   echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/Ignitetch/AdvPhishing \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/hajiali_bot\e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
    echo ""
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
-      echo -e "\e[93m    |A|D|V|A|N|C|E| |P|H|I|S|H|I|N|G| |2|.|0|"      
+      echo -e "\e[93m    |H|a|j|i|A|l|i| |P|H|I|S|H|I|N|G| |2|.|0|"      
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
       echo ""                                                
       }
